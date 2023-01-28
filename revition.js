49a2d1fd05dfa4697dcd087ef578b22457fbbf28
sajedulislamsaju <sajedulislamsaju123@gmail.com>
2023-01-28 15:41:26 +0600

    first commit

---
+++ revition.js
@@ -0,0 +1,13 @@
+var bottle = 'water';
+var colour = 'yellow';
+var number = 67;
+
+
+var Is_expencive = true;
+
+var table_item = ['mobile' , 'mouse' , 'remote' ,'bag' , 'laptop']
+
+
+console.log(table_item)
+
+
---
+++ weile.js
@@ -0,0 +1,9 @@
+//  this is the bia bari khabar ...
+
+
+var roastGiven = 0;
+while(roastGiven < 7) {
+    console.log ('roast de amare please')
+    console.log(roastGiven);
+    roastGiven++;
+}
